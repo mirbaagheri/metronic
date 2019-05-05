c8ad09f4afeff2caaf5e18d048a129df6ea0f9e6
Sayed Mostafa Mirbagheri <mostafamiri65@users.noreply.github.com>
2019-05-05 18:27:21 +0430

    Update config.php
    Reformat config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,5 @@
 <?php
 
-
 return [
 
     'scripts' => [
@@ -17,28 +16,19 @@ return [
         'blocks' => [
 
             'GLOBAL MANDATORY STYLES'   => 'assets/plugins',
-
             'THEME GLOBAL STYLES'       => 'themes/metronic/rtl/assets/global/css',
-
             'THEME LAYOUT STYLES'       => 'themes/metronic/rtl/assets/layouts',
-
             'PAGE LEVEL PLUGINS'        => 'assets/plugins',
         ],
 
     ],
 
     'themeName'         => 'metronic',
-
     'themeVersion'      => '4.6.0',
-
     'pluginsLocation'   => 'assets/plugins',
-
     'layoutsLocation'   => 'assets/layouts',
-
     'layoutName'        => 'layout4',
-
     'minify'            => true,
-
     'pageDirection'     => 'rtl'
 
 ];
